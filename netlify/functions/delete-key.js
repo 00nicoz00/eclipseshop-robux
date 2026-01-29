3f4c2deecfcd5d86baf6a2aa707e571d743ab087
00nicoz00 <eclipsenet00@proton.me>
2026-01-29 02:17:22 +0100

    Update delete-key.js

--- netlify/functions/delete-key.js
+++ netlify/functions/delete-key.js
@@ -1,20 +1,11 @@
-const admin = require("firebase-admin");
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_KEY))
-  });
-}
-
-const db = admin.firestore();
+const db = require("./_firebaseAdmin");
 
 exports.handler = async (event) => {
   const { id } = JSON.parse(event.body);
-
   await db.collection("keys").doc(id).delete();
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ ok: true })
+    body: JSON.stringify({ ok: true }),
   };
 };
