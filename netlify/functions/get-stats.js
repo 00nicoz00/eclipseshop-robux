f4a9ff816e7139f360776502d90a9f5d2cff7c0b
00nicoz00 <eclipsenet00@proton.me>
2026-01-29 02:17:32 +0100

    Update get-stats.js

--- netlify/functions/get-stats.js
+++ netlify/functions/get-stats.js
@@ -1,27 +1,21 @@
-const admin = require("firebase-admin");
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_KEY))
-  });
-}
-
-const db = admin.firestore();
+const db = require("./_firebaseAdmin");
 
 exports.handler = async () => {
   const snap = await db.collection("keys").get();
 
-  let total = 0;
+  let total = snap.size;
   let redeemed = 0;
-  let active = 0;
 
-  snap.forEach(doc => {
-    total++;
-    doc.data().redeemed ? redeemed++ : active++;
+  snap.forEach(d => {
+    if (d.data().redeemed) redeemed++;
   });
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ total, redeemed, active })
+    body: JSON.stringify({
+      total,
+      redeemed,
+      available: total - redeemed
+    }),
   };
 };
