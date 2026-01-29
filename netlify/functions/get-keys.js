fdbc6e5dfd15137647995e74df481d7df5323260
00nicoz00 <eclipsenet00@proton.me>
2026-01-29 02:07:07 +0100

    Create get-keys.js

---
+++ netlify/functions/get-keys.js
@@ -0,0 +1,15 @@
+const admin = require("firebase-admin");
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_KEY))
+  });
+}
+
+const db = admin.firestore();
+
+exports.handler = async () => {
+  const snap = await db.collection("keys").get();
+  const keys = snap.docs.map(d => d.data());
+  return { statusCode: 200, body: JSON.stringify(keys) };
+};
